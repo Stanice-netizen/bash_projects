a4efa1d05c15b19aa86ab4403bd82f072a6e34bb
Losha Stanice <kieramaxswell@gmail.com>
2026-03-30 11:30:23 +0100

    fix: modifying code to count .txt and .sh files

---
+++ loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COUNT=0
+
+for f in *.txt; do
+  if [[ -f "$f" ]] && [[ "$f" == *.txt ]]; then
+     echo "$f is a regular file." 
+     COUNT=$(( $COUNT + 1 ))
+   fi
+done
+
+echo "total is: $COUNT"
+ 
