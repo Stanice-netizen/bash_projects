1270267d12e1a6d902450b54ba0e9d31c8cb90b7
Losha Stanice <kieramaxswell@gmail.com>
2026-03-30 11:06:25 +0100

    chore: bash programming projects

---
+++ password.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+read -p "Enter Password: " password
+echo ""
+echo "Password is: $password"
+
+length=${#password}
+
+if [[ $length -lt 8 ]]; then
+ echo "Password is short. Minimum 8 characters required"
+ exit 1
+fi
+
+if [[ ! "$password" =~ [A-Z] ]]; then
+  echo "Atleast an uppercase character needed"
+  exit 1
+fi
+
+echo " Password is valid"
